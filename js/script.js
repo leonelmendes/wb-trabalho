62496874dff2ef2d6914f76b0426983b31914397
Leonel Francisco <codehub.dev@outlook.com>
2024-10-30 02:26:32 +0000

    Update Menu Geral
    Dei uma melhorada no menu e implementei o menu Hamburguer quando a tela é reduzida, fiz a atualização em todos arquivos html e implementei um documento JS Para ativar o menu hamburguer

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function alterarMenu() {
+    const menu = document.querySelector('.menu');
+    const menuHamburguer = document.querySelector('.menu-hamburguer');
+  
+    menu.classList.toggle('show');
+    menuHamburguer.classList.toggle('active');
+  }
+  
