98010dae3dca61bd6e5376286453f4eaef3830a3
Leonel Francisco <codehub.dev@outlook.com>
2024-11-23 21:27:05 +0000

    Uodate geral/ pagina Checkout, Cart
    criei as paginas que faltavam, e melhorei algumas pagians, so falta alterar o menu para todas as demais paginas para aparecer o cart e o registro

--- js/script.js
+++ js/script.js
@@ -4,5 +4,27 @@ function alterarMenu() {
   
     menu.classList.toggle('show');
     menuHamburguer.classList.toggle('active');
-  }
-  
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.getElementById('checkout-form');
+  const successDialog = document.getElementById('success-dialog');
+  const closeDialogBtn = document.getElementById('close-dialog-btn');
+
+  // Adiciona o evento de 'submit' para o formulário
+  form.addEventListener('submit', function(event) {
+      event.preventDefault(); // Impede o envio real do formulário
+
+      // Abre o diálogo
+      successDialog.showModal();
+  });
+
+  // Adiciona evento para o botão "OK"
+  closeDialogBtn.addEventListener('click', function() {
+      // Fecha o diálogo
+      successDialog.close();
+
+      // Redireciona para a página principal
+      window.location.href = 'principal.html'; // Redirecionamento para a página principal
+  });
+});
